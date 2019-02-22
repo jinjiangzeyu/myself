d721deac3203df43ba6fbab264945e2016c9d213
jinjiang <[495315096@qq.com]>
2019-02-22 14:19:21 +0800

    下载了几个icon!

--- src/components/pages/MyHome.js
+++ src/components/pages/MyHome.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './css/MyHome.scss';
+import Wx from '../../assets/images/wechat.png'
+import Steam from '../../assets/images/steam.png'
+import mail from '../../assets/images/mail.png'
+import Git from '../../assets/images/github.png'
 
 const Home = () => {
   return (
@@ -8,6 +12,10 @@ const Home = () => {
         <div>
           <h1 className='content_name'>I'm JinJiangzeyu</h1>
           <h1 className='content_motto'>Even the darkest night will end and the sun will rise</h1>
+          <img src={Wx} alt=""/>
+          <img src={Steam} alt=""/>
+          <img src={mail} alt=""/>
+          <img src={Git} alt=""/>
         </div>
       </div>
     </div>
